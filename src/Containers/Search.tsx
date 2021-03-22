0c4768ef30f78b0fea489045f2cca764d5699aad
이종호 <substantiation@naver.com>
2021-03-23 08:10:50 +0900

    update Data init

--- src/Containers/Search.tsx
+++ src/Containers/Search.tsx
@@ -24,25 +24,36 @@ export default function Search() {
         page : 0
     })
     
-    const movieTitleSearch = async (title : string, page : number = 1) => {
+    const movieTitleSearchFetch = async (title : string = "",page : number = 1) => {
         const URL = await fetch(`https://www.omdbapi.com/?s=${title}&page=${page}&apikey=ac2eb9f1`);
         //추후 Appkey변경 .
         //s = title
         //page = 1 < n < 100
         const getJson = await URL.json();
-
-        //Todo 동일한  title이 전달 되면 결과 push
-        //Todo 동일하지 않다면 새로운 list제공
-        let list : Array<InterfaceShortResult> = title == searchingResult.title ? 
-            [...searchingResult.list,...getJson] : 
-            [...getJson];
-
+        console.log(getJson)
+        const searchingResult : Array<InterfaceShortResult>  = [...getJson.Search]
+        //문제 : 동일한 title을 검색한 경우, 검색을 허용 할 것인가 ? 
+        //해답 : 동일한 title이라도 DB에 update된 결과가 존재 할수 있다. 
+        return searchingResult;
+    }
+    const movieTitleSearch = async (title : string) => {
+        const getList = await movieTitleSearchFetch(title);
         setSerachingResult({
-            title,
-            list,
-            page
+            title : title,
+            list : getList,
+            page : 1
         })
     } 
+
+    const movieTitleSearchingUpdate = async (title : string,page : number) => {
+        const getList = await movieTitleSearchFetch(title,page);
+        setSerachingResult({
+            title : title,
+            list : [...searchingResult.list,...getList],
+            page : page
+        })
+    }
+
     
     return (
         <SearchContainer>
